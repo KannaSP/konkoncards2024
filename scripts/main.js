01ec7563605c235b5ce19bd7957e885b479a8687
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2024-04-23 20:17:27 +1000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import { wallet_open, wallet_close, wallet_open_coin, wallet_close_coin, 
-coin_init, coin_enters_target, coin_hovering, coin_drop, coin_position_readjust, coin_leaves_target} 
+coin_init, coin_enters_target, coin_hovering, coin_drop, coin_position_readjust, coin_leaves_target } 
 from "./animations.js";
 
 
