272f5cf041a85dbbf2f416dd62e1f55109bea393
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2024-05-11 00:46:22 +0700

    Update animations.js

--- scripts/animations.js
+++ scripts/animations.js
@@ -40,12 +40,12 @@ Source: https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2
 
 export function coin_init(ev){
     ev.dataTransfer.setData("element_class", ev.target.class);
-    document.getElementById("debug_state_display").innerHTML = "init completed";
+    /* document.getElementById("debug_state_display").innerHTML = "init completed"; */
     console.log("coin initialization completed");
 }
 
 export function coin_enters_target(ev){
-    document.getElementById("debug_state_display").innerHTML = "entering target";
+    /* document.getElementById("debug_state_display").innerHTML = "entering target"; */
 }
 
 export function coin_hovering(ev){
@@ -56,12 +56,12 @@ export function coin_drop(ev){
     ev.preventDefault();
     var data = ev.dataTransfer.getData("element_class");
     if(ev.target.id !== "drop_target"){
-        document.getElementById("debug_state_display").innerHTML = "dropped outside target";
+        /* document.getElementById("debug_state_display").innerHTML = "dropped outside target"; */
         document.getElementById("ev_target_id").innerHTML = ev.target.id;
         return 10;
     }
     else {
-        document.getElementById("debug_state_display").innerHTML = "coin dropped";
+        /* document.getElementById("debug_state_display").innerHTML = "coin dropped"; */
         pull_a_card();
         document.getElementById("ev_target_id").innerHTML = ev.target.id;
         console.log("coin dropped");
