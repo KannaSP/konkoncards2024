23fa6e12641d4d7a0384a284084a12b3fe02922a
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2024-05-29 00:56:20 +0700

    Changing pulled value fromstring to actual boolean.

--- card_images/cardlist.js
+++ card_images/cardlist.js
@@ -1,9 +1,9 @@
 const server_card_list = [
-    { "artist" : "Kinoko", "front_art" : "Kinoko_CardArt.png", "back_art" : "Kinoko_CardBack.png", "pulled" : "false" },
-    { "artist" : "fluffaldog", "front_art" : "fluffaldog_thinking_of_fox.jpg", "back_art" : "", "pulled" : "false" },
-    { "artist" : "moh", "front_art" : "2024-moh-CardCharacter.png", "back_art" : "2024-moh-CardLetter.png", "pulled" : "false" },
-    { "artist" : "mofumofu", "front_art" : "Mohumohu_Image.png", "back_art" : "Mohumohu_Letter.png", "pulled" : "false" },
-    { "artist" : "ikki", "front_art" : "FubuKingdom.png", "back_art" : "FubuLetter.png", "pulled" : "false" }
+    { "artist" : "Kinoko", "front_art" : "Kinoko_CardArt.png", "back_art" : "Kinoko_CardBack.png", "pulled" : false },
+    { "artist" : "fluffaldog", "front_art" : "fluffaldog_thinking_of_fox.jpg", "back_art" : "", "pulled" : false },
+    { "artist" : "moh", "front_art" : "2024-moh-CardCharacter.png", "back_art" : "2024-moh-CardLetter.png", "pulled" : false },
+    { "artist" : "mofumofu", "front_art" : "Mohumohu_Image.png", "back_art" : "Mohumohu_Letter.png", "pulled" : false },
+    { "artist" : "ikki", "front_art" : "FubuKingdom.png", "back_art" : "FubuLetter.png", "pulled" : false }
 ];
 
 const remote_update_flag = "2024_05_27update01";
