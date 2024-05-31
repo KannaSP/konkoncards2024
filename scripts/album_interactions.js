9fd32db1c5884a2f41bcd2fcd748fa3dc8208b6f
White Glasses <83710956+bempfoobq052021@users.noreply.github.com>
2024-05-31 19:15:19 +0700

    Small update to the random number and Boris's idea

--- scripts/album_interactions.js
+++ scripts/album_interactions.js
@@ -300,8 +300,8 @@ function populate_visible_element(shelf_number) {
     console.log("shnum : "+shelf_number);
     console.log("cdnum : "+(current_decor_number = 0));
     var random_numbers = [];
-    random_numbers.add(Math.ceil(Math.random() * 12));
-    random_numbers.add((1 + random_number + Math.ceil(Math.random() * 12)) % 12);
+    random_numbers.push(Math.ceil(Math.random() * 12));
+    random_numbers.push((1 + random_number + Math.ceil(Math.random() * 12)) % 12);
     while( counter < cards_per_shelf )
     {
         var working_index = starting_data_number + counter - current_decor_number;
